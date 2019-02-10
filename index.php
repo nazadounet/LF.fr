67ce8a3cb23d77967e5a0244b5bf242d60006974
nazadounet <nazadounet@mbpdenazadounet.home>
2019-02-10 12:31:50 +0100

    Convoyage steps section done

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-    include('include/header.php')
+include('include/header.php')
 ?>
-
-
 
 
     <!--
@@ -26,61 +24,61 @@
                 Logistic Solution gère le convoyage ainsi que le transport par camion de votre parc automobile.
             </span>
 
-                <div class="about_icon_container">
+            <div class="about_icon_container">
 
-                    <div class="row">
+                <div class="row">
 
-                        <div class="col-md">
+                    <div class="col-md">
 
-                            <div class="about_item_icon_container">
+                        <div class="about_item_icon_container">
 
                                 <span class="about_item_icon_title">
                                     Tout véhicule
                                 </span>
 
-                                <div class="about_img_container">
+                            <div class="about_img_container">
 
-                                    <img class="about_item_icon" src="images/about/all_car.png" alt="icon_all_vehicule">
+                                <img class="about_item_icon" src="images/about/all_car.png" alt="icon_all_vehicule">
 
-                                </div>
+                            </div>
 
-                                <span class="about_item_icon_description">
+                            <span class="about_item_icon_description">
                                     Deux roues, voiture, camionette ou encore poid lourd, nous prenons en charge tout type de véhicules
                                 </span>
 
-                            </div>
                         </div>
+                    </div>
 
-                        <div class="col-md">
-                            <div class="about_item_icon_container">
+                    <div class="col-md">
+                        <div class="about_item_icon_container">
                                 <span class="about_item_icon_title">
                                     Sécurité
                                 </span>
-                                <div class="about_img_container">
-                                    <img class="about_item_icon" src="images/about/assurance.png" alt="icon_waranty">
-                                </div>
-                                <span class="about_item_icon_description">
+                            <div class="about_img_container">
+                                <img class="about_item_icon" src="images/about/assurance.png" alt="icon_waranty">
+                            </div>
+                            <span class="about_item_icon_description">
                                     Assurance AXA tout risque pour chacun de vos trajet ainsi que de plaque W.
                                 </span>
-                            </div>
                         </div>
+                    </div>
 
-                        <div class="col-md">
-                            <div class="about_item_icon_container">
+                    <div class="col-md">
+                        <div class="about_item_icon_container">
                                 <span class="about_item_icon_title">
                                     Service express
                                 </span>
-                                <div class="about_img_container">
-                                    <img class="about_item_icon" src="images/about/express.png" alt="icon_express">
-                                </div>
-                                <span class="about_item_icon_description">
+                            <div class="about_img_container">
+                                <img class="about_item_icon" src="images/about/express.png" alt="icon_express">
+                            </div>
+                            <span class="about_item_icon_description">
                                     Besoin d'un départ immédiat? <br>Nous organisons vos courses dans les 2 heures (en Île de France).
                                 </span>
-                            </div>
                         </div>
-
                     </div>
+
                 </div>
+            </div>
 
             <span class="about_section_last_description">
                     Tout les itinéraires sont effectués par des chauffeurs professionnels, formés et agréés.
@@ -97,91 +95,129 @@
         ------------ CONVOYAGE SECTION -----------
         -->
 
-        <div class="convoyage_section_container row">
+        <div class="convoyage_section_container">
 
             <h1>Convoyage</h1>
 
             <div class="convoyage_prologue">
-                Vous avez besoin de faire voyager votre véhicules mais ne souhaitez pas <nobr>le faire par camion?</nobr>
-                <br />
-                Vous souhaitez louer les services d'un conducteur détenteur du <nobr>permis poids lourd?</nobr>
-                <br />
-                Alors faites appel à un de nos <strong><nobr>chauffeur agréé et qualifié !</nobr></strong>
+                Vous avez besoin de faire voyager votre véhicules mais ne souhaitez pas
+                <nobr>le faire par camion?</nobr>
+                <br/>
+                Vous souhaitez louer les services d'un conducteur détenteur du
+                <nobr>permis poids lourd?</nobr>
+                <br/>
+                Alors faites appel à un de nos <strong>
+                    <nobr>chauffeur agréé et qualifié !</nobr>
+                </strong>
             </div>
 
             <h3>Comment ça marche ?</h3>
 
-            <div class="convoyage_steps_container">
+            <div class="convoyage_step_container">
 
-                <!-------------------------------------------------->
+                <div class="row">
 
-                <div class="convoyage_step_one convoyage_step">
+                    <!-------------------------------------------------->
 
-                    <span class="convoyage_steps">
-                        Etape 1
-                    </span>
-                    <br/>
-                    <span class="convoyage_steps_title">
-                        Prise en charge de votre vehicule
-                    </span>
+                    <div class="col-md-4">
 
-                    <img src="images/convoyage/key.png" alt="key">
+                        <div class="convoyage_step_one convoyage_step">
 
-                    <span class="convoyage_steps_description">
-                        <strong>
-                            Voiture, deux roues, camionnette ou encore poids lourd.
-                        </strong>
-                        Logistic Solution achemine porte à porte tous types de véhicule.
-                    </span>
+                            <span class="convoyage_step_number">
+                                Etape 1
+                            </span>
+                                <br/>
+                                <span class="convoyage_step_title">
+                                Prise en charge de votre
+                                <br>
+                                vehicule
+                            </span>
+
+                            <img src="images/convoyage/key.png" alt="key">
+
+                            <span class="convoyage_step_description">
+                                <strong>
+                                    Voiture, deux roues, camionnette ou encore poids lourd.
+                                </strong>
+                                Logistic Solution achemine porte à porte tous types de véhicule.
+                            </span>
+
+                            <span class="convoyage_hr"></span>
+
+                        </div>
+
+
+                    </div>
+
+
+                    <!-------------------------------------------------->
+                    <div class="col-md-4">
+
+                        <div class="convoyage_step_two convoyage_step">
+
+                            <span class="convoyage_step_number">
+                                Etape 2
+                            </span>
+                            <br/>
+                            <span class="convoyage_step_title">
+                                Le chauffeur effectue le trajet
+                                <br>
+                                demandé
+                            </span>
+
+                            <img src="images/convoyage/travel.png" alt="travel_icon">
+
+                            <span class="convoyage_step_description">
+                                Votre véhicule est acheminé au point de rendez-vous, France, Europe et sur d'autres continents.
+                                <strong>
+                                    Tout nos chauffeurs sont formés a l'eco-conduite.
+                                </strong>
+                                Vous bénéficiez d'une assurance tout-risque.
+                            </span>
+
+                            <span class="convoyage_hr"></span>
+
+                        </div>
+
+
+                    </div>
+
+
+                    <!-------------------------------------------------->
+                    <div class="col-md-4">
+
+                        <div class="convoyage_step_three convoyage_step">
+
+                            <span class="convoyage_step_number">
+                                Etape 3
+                            </span>
+                            <br/>
+                            <span class="convoyage_step_title">
+                                Nous le déposons à l'endroit
+                                <br>
+                                souhaité
+                            </span>
+
+                            <img src="images/convoyage/flag.png" alt="flag_icon">
+
+                            <span class="convoyage_step_description">
+                                Nous déposons le véhicule à l'endroit souhaitez,
+                                <strong>
+                                    vous ne vous occupé de rien
+                                </strong>
+                                à par profitez de votre véhicule à votre arrivé sur le lieu de destination.
+                            </span>
+
+                            <span class="convoyage_hr"></span>
+
+                        </div>
+
+
+                    </div>
+
 
                 </div>
 
-                <!-------------------------------------------------->
-
-                <div class="convoyage_step_two convoyage_step">
-
-                    <span class="convoyage_steps">
-                        Etape 2
-                    </span>
-                    <br/>
-                    <span class="convoyage_steps_title">
-                        Le chauffeur effectue le trajet demandé
-                    </span>
-
-                    <img src="images/convoyage/travel.png" alt="travel_icon">
-
-                    <span class="convoyage_steps_description">
-                        Votre véhicule est acheminé au point de rendez-vous, France, Europe et sur d'autres continents.
-                        <strong>
-                            Tout nos chauffeurs sont formés a l'eco-conduite.
-                        </strong>
-                        Vous bénéficiez d'une assurance tout-risque.
-                    </span>
-
-                </div>
-
-                <!-------------------------------------------------->
-
-                <div class="convoyage_step_three convoyage_step">
-
-                    <span class="convoyage_steps">
-                        Etape 3
-                    </span>
-                    <br/>
-                    <span class="convoyage_steps_title">
-                        Nous le déposons à l'endroit souhaité
-                    </span>
-
-                    <img src="images/convoyage/flag.png" alt="flag_icon">
-
-                    <span class="convoyage_steps_description">
-                        Nous déposons le véhicule à l'endroit souhaitez,
-                        <strong>
-                            vous ne vous occupé de rien
-                        </strong>
-                        à par profitez de votre véhicule à votre arrivé sur le lieu de destination.
-                    </span>
-                </div>
             </div>
         </div>
 
@@ -195,5 +231,5 @@
     </div><!--CONTENT-->
 
 <?php
-    include('include/footer.php')
+include('include/footer.php')
 ?>
